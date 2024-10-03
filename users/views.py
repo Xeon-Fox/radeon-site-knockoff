4c535f912c5c07d6806ecef083fa510883d5279b
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-10-03 21:07:54 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+from .forms import RegistrationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  # Hash the password
+            user.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')  # Redirect to a login page or wherever you want
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
