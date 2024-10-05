67366a6a18b54281ecbdb1420c83d5126b3a07f9
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-10-05 11:44:57 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-# from .views import register
-
-=======
 from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
->>>>>>> ayan
 # urlpatterns = [
 #     path('register/', register, name='register'),
 # ]
