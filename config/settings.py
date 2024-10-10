05757ccb7cfaa99c099ba05e380000b625dc1618
ayo <150446051+ayan1232008@users.noreply.github.com>
2024-10-10 21:26:23 +0500

    CSS HERE
    CSS

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "resorces/static/"
-STATICFILES_DIRS = [BASE_DIR / "resorces/static"]
-STATIC_ROOT = BASE_DIR / "resources/staticfiles"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "templates/static/"]
+STATIC_ROOT = BASE_DIR / "/static/"
 
 MEDIA_URL = "resources/media/"
 MEDIA_ROOT = BASE_DIR / "resources/media"
---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomePageView, SearchResultsView
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
+]
