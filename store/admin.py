714cffbf970e81fbd3dec41c39de4895a4399b4c
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-10-11 21:09:43 +0500

    most backend done but not connected

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'gpu', 'quantity')
+    search_fields = ('gpu__title',)
