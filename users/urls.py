2b2b34fd9c531665e67b99e924082eb09da8186f
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-11-10 12:13:43 +0500

    fixes

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,6 @@ from .views import (
 )
 
 from django.urls import path
-from .views import HomeView, ProductsView, RDNAView
 app_name = "users"
 
 urlpatterns = [
@@ -17,7 +16,4 @@ urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("password-change/", UserPasswordChangeView.as_view(), name="password_change"),
-    path('', HomeView.as_view(), name='home'),
-    path('series/', ProductsView.as_view(), name='products'),
-    path('rdna/', RDNAView.as_view(), name='rdna'),
 ]
